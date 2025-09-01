abaf73f29c88b00622b4e12ecd31f3ab266c6c46
汪邹 <v-wangzou@sinochem.com>
2025-09-01 15:59:00 +0800

    Site updated: 2025-09-01 15:58:59

--- js/outdate.js
+++ js/outdate.js
@@ -25,25 +25,3 @@
 //           '</div>' + posts[0].innerHTML;
 //       }
 //     };
-document.addEventListener("DOMContentLoaded", function () {
-  // 拦截文档内所有锚点点击
-  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-    anchor.addEventListener("click", function (e) {
-      e.preventDefault(); // 阻止默认跳转行为
-      const targetId = this.getAttribute("href");
-      if (targetId === "#") return;
-      const targetElement = document.querySelector(targetId);
-      if (targetElement) {
-        // 计算滚动位置，减去固定栏高度
-        const targetPosition = targetElement.offsetTop + 500; // 你的固定导航栏高度
-        // 平滑滚动到目标位置
-        window.scrollTo({
-          top: targetPosition,
-        //   behavior: "smooth",
-        });
-        // 可选：更新浏览器地址栏的哈希值
-        history.replaceState(null, null, targetId);
-      }
-    });
-  });
-});
